6c84ba1ae97790fb057e5fe0d50816730ea3fa65
NadaJankovic <nadajankovic86@gmail.com>
2021-06-02 20:49:24 +0200

    Added basic input validation in case of  any of inputs are empty.

--- frontend/src/components/Edit_PreviewItem.js
+++ frontend/src/components/Edit_PreviewItem.js
@@ -1,91 +1,139 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ServiceContext } from '../services/serviceContext';
-import { TextField, Button, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { TextField, Button, Typography, FormHelperText, FormControl } from '@material-ui/core';
 
 function Edit_PreviewItem(props) {
     const { artItem } = props;
     const { setArtItem } = props;
     const { setShowEditComponent } = props;
-    const {setDepartmentInfo} = props;
+    const { setDepartmentInfo } = props;
     const [showPreview, setShowPreview] = useState(false);
     const [editedArt, setEditedArt] = useState(artItem);
+    // validation state objects
+    const [titleError, setTitleError] = useState({ shortInput: '', notValid: false });
+    const [urlError, setUrlError] = useState({ shortInput: '', notValid: false });
+    const [descriptionError, setDescriptionError] = useState({ shortInput: '', notValid: false });
+    console.log(editedArt.name)
+
+    const validate = () => {
+       
+        let isValid = true;
+        if (editedArt.name === '') {
+            titleError.shortInput = 'Empty Field Not Allowed';
+            titleError.notValid = true;
+            isValid = false
+        } if (editedArt.url === '') {
+            urlError.shortInput = 'Empty Field Not Allowed';
+            urlError.notValid = true;
+            isValid = false
+        } if (editedArt.description === '') {
+            descriptionError.shortInput = 'Empty Field Not Allowed';
+            descriptionError.notValid = true
+            isValid = false
+        }
+        setTitleError(titleError)
+        setUrlError(urlError);
+        setDescriptionError(descriptionError);
+        return isValid
+    }
 
     const handleChange = (e) => {
         setEditedArt({ ...editedArt, [e.target.name]: e.target.value })
     }
-    const saveItem = () => {
-        // find if item exists in storage and update it with edited object
-        let storageCollection = JSON.parse(localStorage.getItem('artItem'));
-        if (!storageCollection) {
-            let update = {
-                id: artItem.id,
-                name: editedArt.name,
-                type: artItem.type,
-                description: editedArt.description,
-                url: editedArt.url
+
+    const saveItem = (e) => {
+        let err = validate();
+        if (!err) {
+            e.preventDefault();
+        } else {
+            // find if item exists in storage and update it with edited object
+            let storageCollection = JSON.parse(localStorage.getItem('artItem'));
+            if (!storageCollection) {
+                let update = {
+                    id: artItem.id,
+                    name: editedArt.name,
+                    type: artItem.type,
+                    description: editedArt.description,
+                    url: editedArt.url
+                }
+                let storage = localStorage.setItem('artItem', JSON.stringify(update));
+                setArtItem(update)
+                setShowEditComponent(false);
             }
-            let storage = localStorage.setItem('artItem', JSON.stringify(update));
-            setArtItem(update)
+            const departInfo = JSON.parse(localStorage.getItem('departInfo'));
+
+            //update department list
+            const filterDepart = departInfo.map(el => {
+                return {
+                    ...el, collection: el.collection.map(item =>
+                        item.id === editedArt.id ?
+                            {
+                                id: item.id,
+                                name: editedArt.name,
+                                type: item.type,
+                                collection: item.collection
+                            } :
+                            item)
+                }
+            })
+            let storage = localStorage.setItem('artItem', JSON.stringify(editedArt));
+            let departStorage = localStorage.setItem('departInfo', JSON.stringify(filterDepart));
+            setArtItem([editedArt])
             setShowEditComponent(false);
         }
-        const departInfo = JSON.parse(localStorage.getItem('departInfo'));
-
-        //update department list
-        const filterDepart = departInfo.map(el => {
-            return {
-                ...el, collection: el.collection.map(item =>
-                    item.id === editedArt.id ?
-                        {
-                            id: item.id,
-                            name: editedArt.name,
-                            type: item.type,
-                            collection:item.collection
-                        } :
-                        item)
-            }
-        })
-        let storage = localStorage.setItem('artItem', JSON.stringify(editedArt));
-        let departStorage = localStorage.setItem('departInfo', JSON.stringify(filterDepart));
-        setArtItem([editedArt])
-        setShowEditComponent(false);
     }
-
     const onPreview = () => {
         setShowPreview(true);
     }
     return (
         <div className='layout-container'>
             <div className='edit-form-container'>
-                <form noValidate >
+                <form onSubmit={saveItem} noValidate >
                     <div className='edit-input-field'>
-                        <TextField
-                            className='input-text'
-                            label="Title"
-                            name='name'
-                            variant="outlined"
-                            onChange={handleChange}
-                            value={editedArt.name}
-                        />
-                        <TextField
-                            className='input-text'
-                            label="Image URL"
-                            name='url'
-                            variant="outlined"
-                            onChange={handleChange}
-                            value={editedArt.url}
-                        />
-                        <TextField
-                            className='input-text'
-                            multiline
-                            label="Description"
-                            name='description'
-                            variant="outlined"
-                            onChange={handleChange}
-                            value={editedArt.description}
-                        />
+                        <FormControl>
+                            <TextField
+                                className='input-text'
+                                label="Title"
+                                name='name'
+                                variant="outlined"
+                                required
+                                onChange={handleChange}
+                                value={editedArt.name}
+                                error={titleError.notValid}
+                                helperText={titleError.shortInput}
+
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                className='input-text'
+                                label="Image URL"
+                                name='url'
+                                variant="outlined"
+                                required
+                                onChange={handleChange}
+                                value={editedArt.url}
+                                error={urlError.notValid}
+                                helperText={titleError.shortInput}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                className='input-text'
+                                multiline
+                                required
+                                label="Description"
+                                name='description'
+                                variant="outlined"
+                                onChange={handleChange}
+                                value={editedArt.description}
+                                error={descriptionError.notValid}
+                                helperText={urlError.shortInput}
+                            />
+                        </FormControl>
+
                     </div>
                     <div className='save-preview-btns'>
-                        <Button className='save-btn' color='primary' variant='contained' onClick={saveItem}>Save</Button>
+                        <Button type='submit' className='save-btn' color='primary' variant='contained' >Save</Button>
                         <Button className='preview-btn' color='primary' variant='outlined' onClick={onPreview}>Preview</Button>
                     </div>
                 </form>
